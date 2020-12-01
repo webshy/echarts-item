6d065d149fcb74f219480c084153eb7fec980519
hsy <haungshaoyuan@shuziguanxing.com>
2020-12-01 10:38:53 +0800

    增加案例

---
+++ map/1.js
@@ -0,0 +1,202 @@
+let arr = [{
+  name: '内外网统一资产管理',
+    "itemStyle": {
+    "normal": {
+      "color": "#2D38C5",
+    }
+  }
+},
+{
+  name: '数据安全',
+    "itemStyle": {
+    "normal": {
+      "color": "#2D38C5",
+    }
+  }
+},
+{
+  name: '漏洞管理',
+    "itemStyle": {
+    "normal": {
+      "color": "#2D38C5",
+    }
+  }
+},
+{
+  name: '地区监管',
+    "itemStyle": {
+    "normal": {
+      "color": "#2D38C5",
+    }
+  }
+},
+{
+  name: '关保合规',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '内网风险管理',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '行业监管',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '互联网信息监控',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '观星台-关基',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '观星台-标准',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '观星台-监管',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '观星台-内网',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '星络-API',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '星规',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '数据管理平台',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '行业舆情监控',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+
+
+{
+  name: '空间测绘平台',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '能力中心',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '企业Saas检测平台',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '资产识别引擎',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '漏洞扫描引擎',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: 'API审计引擎',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '数据加工中心',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: 'Passets',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+},
+{
+  name: '数据泄露检测引擎',
+    "itemStyle": {
+    "normal": {
+      "color": "#637A87",
+    }
+  }
+} ]
